702dc257c2c92bae801069633c8022a5ac237857
tiagosandoval <104266806+tiagosandoval@users.noreply.github.com>
2022-06-20 17:38:43 -0300

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -8,10 +8,10 @@ Hash::Hash(){
     int n = 25; // lambda = 0.8, lambda = clave/n un numero primo al menos 1.3 veces mas de las claves
 }
 
-int Hash::valor_Hash(string llave /*isni*/)
+int Hash::valor_Hash(std::string llave /*isni*/)
 {
     int hash = 0;
-    for(int  = 0; i < llave.lenght(); i++){
+    for(int i = 0; i < llave.length(); i++){
         if((llave[i] != '(') && (llave[i] != ')'))
             hash = hash + (int)llave[i];
     }
