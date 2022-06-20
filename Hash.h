99159344c6301ecd31d9e9ea138a963f27766342
Liu Vaneshka <46787523+liuvaneshka@users.noreply.github.com>
2022-06-19 23:33:15 -0300

    Update Hash.h

--- Hash.h
+++ Hash.h
@@ -6,14 +6,14 @@
 #define HASH_H
 
 template <typename Dato>
-class Hash {
+class Hash{
 
 private:
 
     static const int n = 25; // lambda = 0.8, lambda = clave/n un numero primo al menos 1.3 veces mas de las claves
 
     struct Item{
-        Dato llave;
+        string llave;
         Dato* dato;
         struct Item* siguiente;
     };
@@ -22,7 +22,7 @@ private:
 
 public:
 
-    //post: construye la tabal hash
+    //post: construye la tabla hash
     Hash();
 
     //pre: recibe una cadena
@@ -31,7 +31,7 @@ public:
 
     //pre: recibe una cadena para generar la clave, y el dato a introducir
     //post: agrega un item a la lista
-    void agregar_item(string isni, Dato* dato);
+    void agregar_item(string llave, Dato* dato);
 
     //pre: indice debe ser un entero
     //post: retorna la cantidad de items dentro de un bucket o indice
@@ -50,16 +50,14 @@ public:
 
     //pre: recibe una cadena representando la llave
     //post: elimina el elemento
-    void eliminar_item(string llave);
+    //void eliminar_item(string llave);
 };
 
 template <typename Dato>
 
 Hash<Dato>::Hash(){
     for(int i = 0; i < n; i++){
-        tabla_hash[i] = new lista;
-        //tabla_hash[i]->lista_escritor.inicializar() ;
-        // Lista <Escritor*> *lista_escritores = new Lista<Escritor*> // esto en el parser procesar escritor
+        tabla_hash[i] = new Item;
         tabla_hash[i]->llave = "";
         tabla_hash[i]->dato = nullptr;
         tabla_hash[i]->siguiente = nullptr;
@@ -73,7 +71,7 @@ int Hash<Dato>::valor_Hash(string llave)
     int hash = 0;
     int tamanio = (int)(llave).length();
 
-    for(int i = 1; i < tamanio; i++){
+    for(int i = 1; i < tamanio; i++){//i = 0; remover if;
         if((llave[i] != ')')){
             hash = hash + (int)llave[i];
         }
@@ -128,14 +126,14 @@ void Hash<Dato>::imprimir_tabla() {
     int cantidad_elementos;
     for(int i = 0; i < n; i++){
 
-        cantidad_elementos = cantidad_listas_en_indice(i);
+        cantidad_elementos = cantidad_items_en_indice(i);
 
         std::cout << "Indice = " << i << std::endl;
 
          if(cantidad_elementos > 0){
              std::cout << "******************" << std::endl;
-             imprimir_lista(i);
-             std::cout <<"  cantidad de listas = " << cantidad_elementos << std::endl;
+             imprimir_items(i);
+             std::cout <<"  cantidad de items por indice = " << cantidad_elementos << std::endl;
              std::cout << "******************" << std::endl;
 
          }
@@ -149,7 +147,7 @@ void Hash<Dato>::imprimir_items(int indice) {
         std::cout << indice << "  contiene los siguientes " << std::endl;
 
         while(puntero != nullptr){
-            std::cout << puntero->dato->obtener_nombre() << std::endl;
+            std::cout << puntero->dato << std::endl;//remover el ->obtener_nombre()
             std::cout << puntero->llave << std::endl;
 
             puntero = puntero->siguiente;
@@ -177,19 +175,19 @@ Dato* Hash<Dato>::encontrar_dato(string llave) {
 
     return dato;
 }
-
+/*
 template <typename Dato>
 void Hash<Dato>::eliminar_item(string llave) {
-/*
+
     int indice = Hash(llave);
 
     lista* puntero_eliminar;
     lista* puntero;
     lista* aux;
 
-*/
+
 
 }
-
+*/
 
 #endif
